5b4d2e05b513a0a384ae08a204387155a23cc224
muffinhead03 <125536227+muffinhead03@users.noreply.github.com>
2025-05-20 00:40:01 +0900

    태연님꺼+전역변수 적용 viewTactics_statistics_List.java

--- viewTactics_statistics_List.java
+++ viewTactics_statistics_List.java
@@ -14,14 +14,12 @@ import javax.swing.SwingConstants;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
-import java.sql.*;
 
 public class viewTactics_statistics_List extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTable table;
-	private int idTeam;
 
 	/**
 	 * Launch the application.
@@ -30,7 +28,7 @@ public class viewTactics_statistics_List extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					viewTactics_statistics_List frame = new viewTactics_statistics_List(1);
+					viewTactics_statistics_List frame = new viewTactics_statistics_List();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -42,12 +40,9 @@ public class viewTactics_statistics_List extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public viewTactics_statistics_List(int idTeam) {
-		this.idTeam = idTeam;
+	public viewTactics_statistics_List() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1000, 300);
-		//setBounds(100, 100, 800, 600);
-
+		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
@@ -57,7 +52,7 @@ public class viewTactics_statistics_List extends JFrame {
 		JButton btnNewButton = new JButton("Back");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				new viewTactics_statistics(idTeam).setVisible(true); dispose();
+				new viewTactics_statistics(DKicker.currentTeamId).setVisible(true); dispose();
 			}
 		});
 		btnNewButton.setBounds(6, 6, 117, 29);
@@ -66,11 +61,11 @@ public class viewTactics_statistics_List extends JFrame {
 		JLabel lblNewLabel = new JLabel("전술 사용 경기 목록");
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setFont(new Font("Lucida Grande", Font.BOLD, 18));
-		lblNewLabel.setBounds(6, 33, 988, 29);
+		lblNewLabel.setBounds(6, 33, 438, 29);
 		contentPane.add(lblNewLabel);
 		
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(6, 74, 988, 192);
+		scrollPane.setBounds(6, 74, 438, 192);
 		contentPane.add(scrollPane);
 		
 		table = new JTable();
@@ -82,65 +77,5 @@ public class viewTactics_statistics_List extends JFrame {
 			}
 		));
 		scrollPane.setViewportView(table);
-		
-		
-		
-		loadAllTacticUsages(idTeam);
 	}
-	public void loadAllTacticUsages(int idTeam) {
-	    String[] columnNames = {"전술 ID", "전술 이름", "포메이션", "경기 ID", "경기 날짜", "상대 팀", "득점", "실점", "전술 위치"};
-	    DefaultTableModel model = new DefaultTableModel(columnNames, 0);
-
-	    try {
-	        Connection conn = DBUtil.getConnection();
-
-	        String sql = 
-	            "SELECT Tac.idTactic, Tac.tacticName, Tac.tacticFormation, " +
-	            "       G.idGame, G.dateGame, T.nation AS opponentTeam, " +
-	            "       G.goalFor, G.goalAgainst, '필드 전술' AS usageType " +
-	            "FROM DB2025_GameRec G " +
-	            "JOIN DB2025_Tactics Tac ON G.idField = Tac.idTactic " +
-	            "JOIN DB2025_Team T ON G.idAgainstTeam = T.idTeam " +
-	            "WHERE Tac.idTeam = ? " +
-	            "UNION ALL " +
-	            "SELECT Tac.idTactic, Tac.tacticName, Tac.tacticFormation, " +
-	            "       G.idGame, G.dateGame, T.nation AS opponentTeam, " +
-	            "       G.goalFor, G.goalAgainst, '세트피스 전술' AS usageType " +
-	            "FROM DB2025_GameRec G " +
-	            "JOIN DB2025_Tactics Tac ON G.idSetpiece = Tac.idTactic " +
-	            "JOIN DB2025_Team T ON G.idAgainstTeam = T.idTeam " +
-	            "WHERE Tac.idTeam = ? " +
-	            "ORDER BY idTactic, idGame ";
-
-	        PreparedStatement pstmt = conn.prepareStatement(sql);
-	        pstmt.setInt(1, idTeam);
-	        pstmt.setInt(2, idTeam);
-
-	        ResultSet rs = pstmt.executeQuery();
-
-	        while (rs.next()) {
-	            model.addRow(new Object[]{
-	                rs.getInt("idTactic"),
-	                rs.getString("tacticName"),
-	                rs.getString("tacticFormation"),
-	                rs.getInt("idGame"),
-	                rs.getDate("dateGame"),
-	                rs.getString("opponentTeam"),
-	                rs.getInt("goalFor"),
-	                rs.getInt("goalAgainst"),
-	                rs.getString("usageType")
-	            });
-	        }
-
-	        table.setModel(model);
-
-	        rs.close();
-	        pstmt.close();
-	        conn.close();
-	    } catch (Exception e) {
-	        e.printStackTrace();
-	    }
-	}
-
-	
 }
