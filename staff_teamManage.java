93c9748196368c80a824762155b0c14e022ac71d
muffinhead03 <125536227+muffinhead03@users.noreply.github.com>
2025-05-20 00:14:17 +0900

    전역변수 staff_teamManage.java

--- staff_teamManage.java
+++ staff_teamManage.java
@@ -1,4 +1,4 @@
-package dataKicker;
+package DB2025Team09;
 
 import java.awt.EventQueue;
 
@@ -6,17 +6,18 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import java.awt.GridLayout;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import javax.swing.JLabel;
-import javax.swing.SwingConstants;
-import java.awt.Font;
-import java.awt.GridLayout;
 
-public class staff_teamManage extends JFrame {
+public class staff_teamManage_scoredMost extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private int idTeam;
 
 	/**
 	 * Launch the application.
@@ -25,7 +26,7 @@ public class staff_teamManage extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					staff_teamManage frame = new staff_teamManage();
+					staff_teamManage_scoredMost frame = new staff_teamManage_scoredMost(DKicker.currentTeamId);
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -37,7 +38,7 @@ public class staff_teamManage extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public staff_teamManage() {
+	public staff_teamManage_scoredMost(int idTeam) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
@@ -46,39 +47,55 @@ public class staff_teamManage extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JPanel panel = new JPanel();
-		panel.setBounds(6, 79, 438, 187);
-		contentPane.add(panel);
-		panel.setLayout(new GridLayout(0, 1, 0, 0));
-		
-		JButton btnNewButton_1 = new JButton("우리 팀의 대외 정보 관리");
-		btnNewButton_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				new staff_teamInfoManage().setVisible(true); dispose();
-			}
-		});
-		panel.add(btnNewButton_1);
-		
-		JButton btnNewButton_2 = new JButton("우리 팀이 최다 득점한 상대팀 조회");
-		btnNewButton_2.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				new staff_teamManage_scoredMost().setVisible(true); dispose();			}
-		});
-		panel.add(btnNewButton_2);
-		
 		JButton btnNewButton = new JButton("Back");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				new staff().setVisible(true); dispose();
+				new staff_teamManage(DKicker.currentTeamId).setVisible(true); dispose();
 			}
 		});
 		btnNewButton.setBounds(6, 6, 117, 29);
 		contentPane.add(btnNewButton);
 		
-		JLabel lblNewLabel = new JLabel("팀 관리");
-		lblNewLabel.setFont(new Font("Lucida Grande", Font.BOLD, 18));
+		JLabel lblNewLabel = new JLabel("우리 팀이 최다 득점한 상태팀");
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setBounds(6, 40, 438, 27);
+		lblNewLabel.setFont(new Font("Lucida Grande", Font.BOLD, 18));
+		lblNewLabel.setBounds(6, 32, 438, 29);
 		contentPane.add(lblNewLabel);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(6, 73, 438, 82);
+		contentPane.add(panel);
+		panel.setLayout(new GridLayout(0, 4, 0, 0));
+		
+		JLabel lblNewLabel_1 = new JLabel("상대 팀 ID:");
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.RIGHT);
+		panel.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("");
+		panel.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("국가:");
+		lblNewLabel_1_1.setHorizontalAlignment(SwingConstants.RIGHT);
+		panel.add(lblNewLabel_1_1);
+		
+		JLabel lblNewLabel_1_2 = new JLabel("");
+		lblNewLabel_1_2.setHorizontalAlignment(SwingConstants.CENTER);
+		panel.add(lblNewLabel_1_2);
+		
+		JLabel lblNewLabel_1_3 = new JLabel("경기 수:");
+		lblNewLabel_1_3.setHorizontalAlignment(SwingConstants.RIGHT);
+		panel.add(lblNewLabel_1_3);
+		
+		JLabel lblNewLabel_1_4 = new JLabel("");
+		lblNewLabel_1_4.setHorizontalAlignment(SwingConstants.CENTER);
+		panel.add(lblNewLabel_1_4);
+		
+		JLabel lblNewLabel_1_5 = new JLabel("평균 득점:");
+		lblNewLabel_1_5.setHorizontalAlignment(SwingConstants.RIGHT);
+		panel.add(lblNewLabel_1_5);
+		
+		JLabel lblNewLabel_1_6 = new JLabel("");
+		lblNewLabel_1_6.setHorizontalAlignment(SwingConstants.CENTER);
+		panel.add(lblNewLabel_1_6);
 	}
 }
