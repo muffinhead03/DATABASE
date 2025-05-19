23cc3382fbd6154ad9afeb4e37336de48325ebc5
muffinhead03 <125536227+muffinhead03@users.noreply.github.com>
2025-05-20 00:41:37 +0900

    태연님꺼+전역변수 re viewTactics_statistics_Achieved.java

--- viewTactics_statistics_Achieved.java
+++ viewTactics_statistics_Achieved.java
@@ -2,6 +2,7 @@ package DB2025Team09;
 
 import java.awt.EventQueue;
 
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -9,20 +10,17 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.SwingConstants;
 import java.awt.Font;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
-import java.sql.*;
 
 public class viewTactics_statistics_Achieved extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTable table;
-	private int idTeam;
 
 	/**
 	 * Launch the application.
@@ -31,7 +29,7 @@ public class viewTactics_statistics_Achieved extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					viewTactics_statistics_Achieved frame = new viewTactics_statistics_Achieved(1);
+					viewTactics_statistics_Achieved frame = new viewTactics_statistics_Achieved();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -43,8 +41,7 @@ public class viewTactics_statistics_Achieved extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public viewTactics_statistics_Achieved(int idTeam) {
-		this.idTeam = idTeam;
+	public viewTactics_statistics_Achieved() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
@@ -56,7 +53,7 @@ public class viewTactics_statistics_Achieved extends JFrame {
 		JButton btnNewButton = new JButton("Back");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				new viewTactics_statistics(idTeam).setVisible(true); dispose();
+				new viewTactics_statistics(DKicker.currentTeamId).setVisible(true); dispose();
 			}
 			
 		});
@@ -82,74 +79,6 @@ public class viewTactics_statistics_Achieved extends JFrame {
 			}
 		));
 		scrollPane.setViewportView(table);
-		
-		loadAchievedStatisticsToTable(table);
-
-	}
-	public void loadAchievedStatisticsToTable(JTable table) {
-	    String[] columnNames = {"전술 ID", "전술 이름", "사용 횟수", "평균 득점", "평균 실점", "평균 슈팅 수", "평균 패스 수"};
-	    DefaultTableModel model = new DefaultTableModel(columnNames, 0);
-
-	    try {
-	        Connection conn = DBUtil.getConnection();
-	        String sql = 
-	        	    "SELECT T.idTactic, T.tacticName, COUNT(*) AS useCount, " +
-	        	    "       AVG(G.goalFor) AS avgGoals, AVG(G.goalAgainst) AS avgGoalsAgainst, " +
-	        	    "       AVG(S.allShots) AS avgShots, AVG(S.accPass) AS avgPasses " +
-	        	    "FROM DB2025_GameRec G " +
-	        	    "JOIN DB2025_GameStat S ON G.idGame = S.idGame " +
-	        	    "JOIN DB2025_Tactics T ON G.idField = T.idTactic " +
-	        	    "WHERE T.idTeam = ? " +
-	        	    "GROUP BY T.idTactic, T.tacticName " +
-	        	    "UNION ALL " +
-	        	    "SELECT T.idTactic, T.tacticName, COUNT(*) AS useCount, " +
-	        	    "       AVG(G.goalFor) AS avgGoals, AVG(G.goalAgainst) AS avgGoalsAgainst, " +
-	        	    "       AVG(S.allShots) AS avgShots, AVG(S.accPass) AS avgPasses " +
-	        	    "FROM DB2025_GameRec G " +
-	        	    "JOIN DB2025_GameStat S ON G.idGame = S.idGame " +
-	        	    "JOIN DB2025_Tactics T ON G.idSetpiece = T.idTactic " +
-	        	    "WHERE T.idTeam = ? " +
-	        	    "GROUP BY T.idTactic, T.tacticName " +
-	        	    "ORDER BY useCount DESC";
-
-
-
-	        PreparedStatement pstmt = conn.prepareStatement(sql);
-	        pstmt.setInt(1, idTeam);
-	        pstmt.setInt(2, idTeam);
-
-	        ResultSet rs = pstmt.executeQuery();
-
-	        while (rs.next()) {
-	            int idTactic = rs.getInt("idTactic");
-	            String tacticName = rs.getString("tacticName");
-	            int useCount = rs.getInt("useCount");
-	            double avgGoals = rs.getDouble("avgGoals");
-	            double avgAgainst = rs.getDouble("avgGoalsAgainst");
-	            double avgShots = rs.getDouble("avgShots");
-	            double avgPasses = rs.getDouble("avgPasses");
-
-	            model.addRow(new Object[]{
-	                idTactic,
-	                tacticName,
-	                useCount,
-	                String.format("%.2f", avgGoals),
-	                String.format("%.2f", avgAgainst),
-	                String.format("%.2f", avgShots),
-	                String.format("%.2f", avgPasses)
-	            });
-	        }
-
-	        rs.close();
-	        pstmt.close();
-	        conn.close();
-
-	        table.setModel(model);
-
-	    } catch (Exception e) {
-	        e.printStackTrace();
-	        JOptionPane.showMessageDialog(null, "전술 성과 데이터를 불러오는 데 실패했습니다.");
-	    }
 	}
 
 }
