f243d3c5000151f3df16e3a178063ca314be6368
muffinhead03 <125536227+muffinhead03@users.noreply.github.com>
2025-05-27 21:22:17 +0900

    주석 staff_gameManage.java

--- staff_gameManage.java
+++ staff_gameManage.java
@@ -25,6 +25,7 @@ public class staff_gameManage extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
+					//teamId 0으로 테스트 실행합니다. 
 					staff_gameManage frame = new staff_gameManage(0);
 					frame.setVisible(true);
 				} catch (Exception e) {
@@ -50,46 +51,57 @@ public class staff_gameManage extends JFrame {
 		panel.setBounds(6, 69, 438, 197);
 		contentPane.add(panel);
 		panel.setLayout(new GridLayout(0, 1, 0, 0));
-		
+
+		//경기 기록 생성하는 버튼입니다. 
 		JButton button = new JButton("경기 기록 생성");
 		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				//경기 생성 화면으로 이동합니다.
 				new staff_gameCreate(idTeam).setVisible(true); dispose();
 			}
 		});
 		panel.add(button);
-		
+
+		//경기 기록 수정하는 버튼입니다. 
 		JButton btnNewButton_1 = new JButton("경기 기록 수정");
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				//경기 수정 화면으로 이동합니다. 
 				new staff_gameSearch_Edit(idTeam).setVisible(true); dispose();
 			}
 		});
-		
+
+		//경기 통계 생성하는 버튼입니다. 
 		JButton btnNewButton_2 = new JButton("경기 통계 데이터 생성");
 		btnNewButton_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				//통계를 생성하는 화면(staff_gameStatisticsCreate(idTeam))으로 이동한다
 				new staff_gameStatisticsCreate(idTeam).setVisible(true); dispose();
 			}
 		});
 		panel.add(btnNewButton_2);
-		
+
+		//출전 선수 명단을 생성하는 버튼입니다. 
 		JButton btnNewButton_3 = new JButton("경기 출전 선수 명단 생성");
 		btnNewButton_3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				//선수 명단 입력 화면(staff_gamePlayerListCreate(idTeam)으로 이동합니다.
 				new staff_gamePlayerListCreate(idTeam).setVisible(true); dispose();
 			}
 		});
 		panel.add(btnNewButton_3);
 		panel.add(btnNewButton_1);
-		
+
+		//팀 간의 경기 요약을 볼 수 있습니다. 
 		JButton btnNewButton_4 = new JButton("다른 팀과 치른 경기 요약");
 		btnNewButton_4.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				//팀 간 경기 요약 화면(staff_gameWithOtherTeams(idTeam))으로 이동합니다.. 
 				new staff_gameWithOtherTeams(idTeam).setVisible(true); dispose();
 			}
 		});
 		panel.add(btnNewButton_4);
+
 		
 		JButton btnNewButton = new JButton("Back");
 		btnNewButton.addActionListener(new ActionListener() {
@@ -99,7 +111,8 @@ public class staff_gameManage extends JFrame {
 		});
 		btnNewButton.setBounds(6, 6, 117, 29);
 		contentPane.add(btnNewButton);
-		
+
+		//타이틀 레벨입니다. 
 		JLabel lblNewLabel = new JLabel("경기 기록 관리");
 		lblNewLabel.setFont(new Font("Lucida Grande", Font.BOLD, 18));
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
