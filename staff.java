1584fad5ab4e35920a6fa58d8732ea0158a5e733
muffinhead03 <125536227+muffinhead03@users.noreply.github.com>
2025-05-27 17:06:20 +0900

    Update staff.java

--- staff.java
+++ staff.java
@@ -38,7 +38,7 @@ public class staff extends JFrame {
 	 */
 	public staff(int idTeam) {
 		
-        
+        	//idTeam은 자신이 고른 팀 id를 표시하고, 그를 이용해서 staff(int idTeam)함수를 수행한다. 
 
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -53,15 +53,18 @@ public class staff extends JFrame {
 		panel.setBounds(6, 41, 438, 225);
 		contentPane.add(panel);
 		panel.setLayout(new GridLayout(0, 2));
-		
+
+		//선수 정보 버튼입니다. 
 		JButton btnNewButton_1 = new JButton("선수 정보");
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				new viewPlayers(idTeam).setVisible(true); dispose();
+					//선수정보 창을 열 수 있습니다
 			}
 		});
 		panel.add(btnNewButton_1);
-		
+
+		//선수 관리를 관리하는 창을 띄우는 버튼이다.
 		JButton btnNewButton_2 = new JButton("선수 관리");
 		btnNewButton_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -69,7 +72,8 @@ public class staff extends JFrame {
 			}
 		});
 		panel.add(btnNewButton_2);
-		
+
+		//팀 정보를 알 수 있는 창을 띄우는 버튼이다. 
 		JButton btnNewButton_3 = new JButton("팀 정보");
 		btnNewButton_3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -77,7 +81,8 @@ public class staff extends JFrame {
 			}
 		});
 		panel.add(btnNewButton_3);
-		
+
+		//팀 관리하는 창을 띄우는 버튼이다. 
 		JButton btnNewButton_4 = new JButton("팀 관리");
 		btnNewButton_4.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -85,7 +90,8 @@ public class staff extends JFrame {
 			}
 		});
 		panel.add(btnNewButton_4);
-		
+
+		//경기 기록 관련
 		JButton btnNewButton_5 = new JButton("경기 기록");
 		btnNewButton_5.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -93,6 +99,7 @@ public class staff extends JFrame {
 			}
 		});
 		panel.add(btnNewButton_5);
+
 		
 		JButton btnNewButton_7 = new JButton("경기 기록 관리");
 		btnNewButton_7.addActionListener(new ActionListener() {
