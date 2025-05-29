79f913431067f2327b9f98ccc5f303b1797b668e
muffinhead03 <125536227+muffinhead03@users.noreply.github.com>
2025-05-29 22:31:57 +0900

    주석문 viewTactics_statistics.java

--- viewTactics_statistics.java
+++ viewTactics_statistics.java
@@ -13,14 +13,13 @@ import javax.swing.SwingConstants;
 import java.awt.Font;
 import java.awt.GridLayout;
 
+//전술별 경기 통계 분석 화면 클래스입니다. 전술별 경기 목록과 성과 요약 화면으로 이동할 수 있습니다.
 public class viewTactics_statistics extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	
-	/**
-	 * Launch the application.
-	 */
+	// 테스트용 메인함수입니다. 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -34,9 +33,7 @@ public class viewTactics_statistics extends JFrame {
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
+	//사용자가 현재 로그인한 팀 ID를 매개변수로 받는 생성자입니다. 
 	public viewTactics_statistics(int idTeam) {
 	
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -50,31 +47,7 @@ public class viewTactics_statistics extends JFrame {
 		JButton btnNewButton = new JButton("Back");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				new viewTactics(idTeam).setVisible(true); dispose();
-			}
-		});
-		btnNewButton.setBounds(6, 6, 117, 29);
-		contentPane.add(btnNewButton);
-		
-		JLabel lblNewLabel = new JLabel("전술별 경기 통계 분석");
-		lblNewLabel.setFont(new Font("Lucida Grande", Font.BOLD, 18));
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setBounds(6, 30, 438, 29);
-		contentPane.add(lblNewLabel);
-		
-		JPanel panel = new JPanel();
-		panel.setBounds(6, 71, 438, 195);
-		contentPane.add(panel);
-		panel.setLayout(new GridLayout(0, 1, 0, 0));
-		
-		JButton btnNewButton_1 = new JButton("전술 사용 경기 목록");
-		btnNewButton_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				new viewTactics_statistics_List(idTeam).setVisible(true); dispose();
-			}
-		});
-		panel.add(btnNewButton_1);
-		
+				new viewTactics(idTeam).setVisible(tr다
 		JButton btnNewButton_2 = new JButton("전술별 성과 요약");
 		btnNewButton_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
