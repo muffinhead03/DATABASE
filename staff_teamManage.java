17c71377a7dee4ab3ba1bd90d563210602310f28
muffinhead03 <125536227+muffinhead03@users.noreply.github.com>
2025-05-29 20:45:49 +0900

    주석 staff_teamManage.java

--- staff_teamManage.java
+++ staff_teamManage.java
@@ -13,15 +13,14 @@ import javax.swing.SwingConstants;
 import java.awt.Font;
 import java.awt.GridLayout;
 
+//팀 관련 기능을 관리하는 메인 메뉴 프레임입니다.
 public class staff_teamManage extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private int idTeam;
 
-	/**
-	 * Launch the application.
-	 */
+	// 테스트 실행용 메인 메서드입니다. (idTeam =1 이며, 실제로 실행되진 않습니다. )
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -35,9 +34,7 @@ public class staff_teamManage extends JFrame {
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
+	
 	public staff_teamManage(int idTeam) {
 		this.idTeam = idTeam;
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -47,12 +44,14 @@ public class staff_teamManage extends JFrame {
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
+		//메뉴 버튼들이 들어갈 패널입니다. 
 		JPanel panel = new JPanel();
 		panel.setBounds(6, 79, 438, 187);
 		contentPane.add(panel);
 		panel.setLayout(new GridLayout(0, 1, 0, 0));
-		
+
+		//우리 팀의 대외 정보 관리창입니다. DB를 수정하는 기능을 가집니다. 
 		JButton btnNewButton_1 = new JButton("우리 팀의 대외 정보 관리");
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -60,7 +59,8 @@ public class staff_teamManage extends JFrame {
 			}
 		});
 		panel.add(btnNewButton_1);
-		
+
+		//우리 팀이 최다 득점한 상대팀을 조회할 수 있으며, SELECT 쿼리를 사용하여 DB를 조회하는 기능이 있습니다.
 		JButton btnNewButton_2 = new JButton("우리 팀이 최다 득점한 상대팀 조회");
 		btnNewButton_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -76,7 +76,8 @@ public class staff_teamManage extends JFrame {
 		});
 		btnNewButton.setBounds(6, 6, 117, 29);
 		contentPane.add(btnNewButton);
-		
+
+		//메뉴 타이틀 라벨입니다. 
 		JLabel lblNewLabel = new JLabel("팀 관리");
 		lblNewLabel.setFont(new Font("Lucida Grande", Font.BOLD, 18));
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
